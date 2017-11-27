413ae1781fdf2efaf7881243401b3fb5d45fab5c
Gwenola LAINE <laineg.contact@gmail.com>
2017-11-27 10:28:50 +0100

    methode delete

--- src/main/java/dev/gestionmissions/controller/MissionController.java
+++ src/main/java/dev/gestionmissions/controller/MissionController.java
@@ -10,6 +10,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -66,4 +67,10 @@ public class MissionController {
 		this.missionRepository.save(m);
 		return true;
 	}
+	
+
+	 @DeleteMapping(value="/{id}")
+	 public void deleteCollegue(@PathVariable int id) {
+		this.missionRepository.delete(this.missionRepository.findOne(id));
+	 }
 }
