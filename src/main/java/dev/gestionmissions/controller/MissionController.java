2afee84e831a132ed3e1e505e5bb215395977e5d
kevin <kev.aubree@gmail.com>
2017-11-28 16:23:01 +0100

    first commit

--- src/main/java/dev/gestionmissions/controller/MissionController.java
+++ src/main/java/dev/gestionmissions/controller/MissionController.java
@@ -2,6 +2,7 @@ package dev.gestionmissions.controller;
 
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,11 +10,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.gestionmissions.entity.Mission;
+import dev.gestionmissions.entity.Statut;
 import dev.gestionmissions.repository.MissionRepository;
 import dev.gestionmissions.repository.NatureRepository;
 import dev.gestionmissions.service.MissionService;
@@ -48,7 +51,22 @@ public class MissionController {
 		return this.missionService.sauvegarderMission(mission);
 	}
 	
-
+	@SuppressWarnings("static-access")
+	@PutMapping(value="/{id}")
+	 public Mission changerStatutMission(@PathVariable int id, @RequestBody Map<String, String> statut) {
+		
+		Mission mission = this.missionRepository.findOne(id);
+		if( statut.get("statut").equals("accepte")) {
+			mission.setStatut(Statut.VALIDEE);
+		}
+		else if (statut.get("statut").equals("rejetee")) {
+			mission.setStatut(Statut.REJETEE);
+		}
+		
+		return missionRepository.save(mission);
+		
+	 }
+	
 	 @DeleteMapping(value="/{id}")
 	 public List<Mission> deleteCollegue(@PathVariable int id) {
 		this.missionRepository.delete(this.missionRepository.findOne(id));
