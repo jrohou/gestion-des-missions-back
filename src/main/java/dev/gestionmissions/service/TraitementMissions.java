cb91a6a7a3bc5cdfc32c90998123fff1d43aae37
Benjamin Lasseur <benjamin.lasseur@gmail.com>
2017-12-05 12:02:13 +0100

    Envoi des mails aux managers

--- src/main/java/dev/gestionmissions/service/TraitementMissions.java
+++ src/main/java/dev/gestionmissions/service/TraitementMissions.java
@@ -1,14 +1,12 @@
 package dev.gestionmissions.service;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.mail.MailSenderValidatorAutoConfiguration;
-import org.springframework.mail.MailException;
-import org.springframework.mail.MailSender;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -29,6 +27,9 @@ public class TraitementMissions {
 
 	@Autowired
 	private EmailServiceImpl email;
+	
+	@Autowired
+	private UserService userService;
 
 	@Scheduled(cron = "0 0 0 * * *") // tous les jours à minuit
 	public void scheduledTask() {
@@ -36,8 +37,18 @@ public class TraitementMissions {
 
 		missions.stream().filter(mission -> mission.getStatut().equals(Statut.INITIALE)).forEach(mission -> {
 			mission.setStatut(Statut.EN_ATTENTE_VALIDATION);
-			email.sendSimpleMessage("benjamin.lasseur@gmail.com", "Nouvelle mission à valider", "La mission "
-					+ mission.getId() + " de l'employé " + mission.getMatricule() + " est en attente de validation.");
+			Optional<String> optEmail = null;
+			String emailManager = "";
+			try {
+				optEmail = userService.findUsers().values().stream().filter(user->user.getSubalternes().contains(mission.getMatricule())).findFirst().map(user->user.getEmail());
+				if(optEmail.isPresent()){
+					emailManager=optEmail.get();
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			email.sendSimpleMessage(emailManager, "Nouvelle mission à valider", "La mission "+ mission.getId() + " de l'employé " + mission.getMatricule() + " est en attente de validation.");
 			this.missionRepository.save(mission);
 		});
 
